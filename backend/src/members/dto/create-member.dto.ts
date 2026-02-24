10554e4c0b8b219f3f7000feea5e48e44df4b97f
Muhammad Ashraf <nayeemashraf92@gmail.com>
2026-02-24 14:45:31 +0500

    password delete in member dto

--- backend/src/members/dto/create-member.dto.ts
+++ backend/src/members/dto/create-member.dto.ts
@@ -3,7 +3,7 @@ import { IsNotEmpty, IsNumber, IsString } from "class-validator";
 export class CreateMemberDto {
   @IsString()
   @IsNotEmpty()
-  fullName: string;   // ← match Entity
+  fullName: string;
 
   @IsString()
   @IsNotEmpty()
